3cd602811d32f82993d7666a76aafecbfa0826cd
Pawel Sobocinski <pawel.sobocinski@atos.net>
2020-03-23 09:48:29 +0100

    Grammar graph/parser for automatic highlighting [VRM-26][VRM-35]

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -489,6 +489,10 @@ module.exports = function (webpackEnv) {
             // This loader doesn't use a "test" so it will catch all modules
             // that fall through the other loaders.
             {
+              test: /\.g4$/i,
+              use: 'raw-loader',
+            },
+            {
               loader: require.resolve('file-loader'),
               // Exclude `js` files to keep "css" loader working as it injects
               // its runtime that would otherwise be processed through "file" loader.

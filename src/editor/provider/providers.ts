e5c64f632bcca3837f1a0dba71232b5eb4bfd649
Pawel Sobocinski <pawel.sobocinski@atos.net>
2020-04-15 14:43:17 +0200

    Added different color for comment in VTL color theme [VRM-24]

--- src/editor/provider/providers.ts
+++ src/editor/provider/providers.ts
@@ -1,18 +1,18 @@
 import * as EditorApi from "monaco-editor";
-import {getSuggestionListByVersion} from "./suggestionsProvider";
-import {VTL_VERSION} from "../settings";
-
+import { getSuggestionListByVersion } from "./suggestionsProvider";
+import { VTL_VERSION } from "../settings";
 
 export const getSuggestions = (version: VTL_VERSION, monaco: typeof EditorApi) => {
     return getSuggestionListByVersion(version, monaco)
 };
 
-export const getVtlTheme = () : EditorApi.editor.IStandaloneThemeData => {
+export const getVtlTheme = (): EditorApi.editor.IStandaloneThemeData => {
     return {
         base: 'vs',
         inherit: true,
         rules: [
             {token: 'string', foreground: '018B03'},
+            {token: 'comment', foreground: '939393'},
             {token: 'operator', foreground: '8B3301'},
             {token: 'operator.special', foreground: '8B3301', fontStyle: 'bold'},
         ],
